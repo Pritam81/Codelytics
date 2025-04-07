ebd187838d2fb38da69880eb43fe69f2412024e2
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-04-07 15:19:07 +0530

    update: Login page ui updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -32,7 +32,7 @@ class _loginscreenState extends State<loginscreen> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 30),
             child: Container(
-              height: height * 0.5,
+              height: height * 0.46,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
@@ -44,107 +44,115 @@ class _loginscreenState extends State<loginscreen> {
                   ),
                 ],
               ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 25.0,
-                      left: 25,
-                      right: 25,
-                      bottom: 10,
-                    ),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                        labelStyle: const TextStyle(color: Colors.black54),
-                        prefixIcon: const Icon(
-                          Icons.person,
-                          color: Colors.black54,
-                        ),
-                        enabled: true,
-
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(color: Colors.grey),
-                        ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 25.0,
+                        left: 25,
+                        right: 25,
+                        bottom: 10,
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25, right: 25),
-                    child: TextFormField(
-                      obscureText: hide,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: const TextStyle(color: Colors.black54),
-                        prefixIcon: const Icon(
-                          Icons.lock,
-                          color: Colors.black54,
-                        ),
-
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            hide ? Icons.visibility : Icons.visibility_off,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          labelStyle: const TextStyle(color: Colors.black54),
+                          prefixIcon: const Icon(
+                            Icons.person,
                             color: Colors.black54,
                           ),
-                          onPressed: () {
-                            setState(() {
-                              hide = !hide;
-                            });
-                          },
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(color: Colors.grey),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: height * 0.03),
+                          enabled: true,
 
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: primary,
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 15,
-                        horizontal: 109,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(color: Colors.grey),
+                          ),
+                        ),
                       ),
                     ),
-                    child: const Text('Login'),
-                  ),
-                  SizedBox(height: height * 0.009),
-                  Text(
-                    "or you can sign in with",
-                    style: TextStyle(color: Colors.black54, fontSize: 12),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25, right: 25),
+                      child: TextFormField(
+                        obscureText: hide,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(color: Colors.black54),
+                          prefixIcon: const Icon(
+                            Icons.lock,
+                            color: Colors.black54,
+                          ),
 
-                  SizedBox(height: height * 0.02),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset('assets/images/google_logo.png', width: 32),
-                        SizedBox(width: width * 0.09),
-                        Image.asset('assets/images/github_logo.png', width: 35),
-                        SizedBox(width: width * 0.09),
-                        Image.asset(
-                          'assets/images/facebook_logo.png',
-                          width: 32,
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              hide ? Icons.visibility : Icons.visibility_off,
+                              color: Colors.black54,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                hide = !hide;
+                              });
+                            },
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(color: Colors.grey),
+                          ),
                         ),
-                        SizedBox(width: width * 0.09),
-                        Image.asset(
-                          'assets/images/linkedin_logo.png',
-                          width: 40,
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: height * 0.03),
+
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: primary,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 15,
+                          horizontal: 109,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      child: const Text('Login'),
+                    ),
+                    SizedBox(height: height * 0.009),
+                    Text(
+                      "or you can sign in with",
+                      style: TextStyle(color: Colors.black54, fontSize: 12),
+                    ),
+
+                    SizedBox(height: height * 0.02),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/google_logo.png',
+                            width: 32,
+                          ),
+                          SizedBox(width: width * 0.09),
+                          Image.asset(
+                            'assets/images/github_logo.png',
+                            width: 35,
+                          ),
+                          SizedBox(width: width * 0.09),
+                          Image.asset(
+                            'assets/images/facebook_logo.png',
+                            width: 32,
+                          ),
+                          SizedBox(width: width * 0.09),
+                          Image.asset(
+                            'assets/images/linkedin_logo.png',
+                            width: 40,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
